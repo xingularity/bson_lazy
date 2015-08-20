e218b1013f5f4f2a7da0156c4e567fb41378ab2f
Zong-han, Xie <icbm0926@gmail.com>
2015-08-20 13:51:15 +0800

    This is my own python3 version of bson_lazy.
    This version also adds a jump function. This functions jumps the file
    handler with various number of json objects

---
+++ __init__.py
@@ -0,0 +1,51 @@
+"""
+Lazy BSON reader.
+"""
+
+import bson
+import struct
+
+S_INT32 = 4
+ZERO = bson.ZERO
+
+def load(fh, as_class=dict,
+              tz_aware=True, uuid_subtype=bson.OLD_UUID_SUBTYPE):
+    """Decode BSON data to multiple documents.
+
+    `fh` must be a file-like object of concatenated, valid, 
+    BSON-encoded documents.
+
+    :Parameters:
+      - `fh`: a file-like object supporting ``.read()``
+      - `as_class` (optional): the class to use for the resulting
+        documents
+      - `tz_aware` (optional): if ``True``, return timezone-aware
+        :class:`~datetime.datetime` instances
+    """
+    while True:
+        obj_size = fh.read(S_INT32)
+        if len(obj_size) == 0:
+            return
+        
+        obj_size = struct.unpack("<i", obj_size)[0]
+        data = fh.read(obj_size - S_INT32)
+
+        if len(data) + S_INT32 < obj_size:
+            raise bson.InvalidBSON("objsize too large")
+        #if bytes(data[-1]) != ZERO:
+        #    raise bson.InvalidBSON("bad eoo")
+        if data[-1] != 0:
+            raise bson.InvalidBSON("bad eoo")
+        
+        elements = data[:-1]
+        yield bson._elements_to_dict(elements, as_class, tz_aware, uuid_subtype)
+
+def jump(fh, number):
+    index = 0
+    while index < number:
+        obj_size = fh.read(S_INT32)
+        if len(obj_size) == 0:
+            return
+        
+        obj_size = struct.unpack("<i", obj_size)[0]
+        fh.read(obj_size - S_INT32)
